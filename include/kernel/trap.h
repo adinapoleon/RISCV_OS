ceb7a1c0b00380e0181d6df6190b21aeb32a7a81
adinapoleon <adi22122004@gmail.com>
2026-05-06 16:12:42 -0500

    made transition from M-Mode to S-Mode

--- include/kernel/trap.h
+++ include/kernel/trap.h
@@ -5,7 +5,7 @@ struct TrapFrame {
     uint32_t regs[31]; // x1 -x31 (x0 is hardwired to zero)
 };
 
-//mcause codes for exceptions and interrupts
+// Trap cause codes for exceptions and interrupts.
 enum class ExceptionCode : uint32_t {
     InstructionAddressMisaligned = 0,
     InstructionAccessFault = 1,
@@ -23,15 +23,19 @@ enum class ExceptionCode : uint32_t {
     StoreAMOPageFault = 15
 };
 
-//mcause codes for interrupts
+// Interrupt cause codes. Machine and supervisor causes use different numbers.
 enum class InterruptCode : uint32_t {
+    SupervisorSoftwareInterrupt = 1,
     MachineSoftwareInterrupt = 3,
+    SupervisorTimerInterrupt = 5,
     MachineTimerInterrupt = 7,
+    SupervisorExternalInterrupt = 9,
     MachineExternalInterrupt = 11
 };
 
-void handle_interrupt(uint32_t code, uint32_t mepc, uint32_t mtval, TrapFrame* frame);
-void handle_exception(uint32_t code, uint32_t mepc, uint32_t mtval, TrapFrame* frame);
+void handle_interrupt(uint32_t code, uint32_t epc, uint32_t tval, TrapFrame* frame);
+void handle_exception(uint32_t code, uint32_t epc, uint32_t tval, TrapFrame* frame);
 
-//trap dispatcher
-extern "C" void trap_handler(uint32_t mcause, uint32_t mepc, uint32_t mtval, TrapFrame* frame);
+// Trap dispatchers.
+extern "C" void supervisor_trap_handler(uint32_t scause, uint32_t sepc, uint32_t stval, TrapFrame* frame);
+extern "C" void machine_trap_handler(uint32_t mcause, uint32_t mepc, uint32_t mtval, TrapFrame* frame);
--- src/kernel/trap.cpp
+++ src/kernel/trap.cpp
@@ -1,112 +1,135 @@
 #include "kernel/trap.h"
 #include "drivers/uart.h"
 
-// for now just print the trap info and advance mepc to avoid infinite traps
-static void advance_mepc() {
+static void advance_sepc() {
     asm volatile(
-        "csrr t0, mepc\n"
+        "csrr t0, sepc\n"
         "addi t0, t0, 4\n"
-        "csrw mepc, t0\n"
+        "csrw sepc, t0\n"
         :
         :
         : "t0"
     );
 }
 
-void handle_interrupt(uint32_t code, uint32_t mepc, uint32_t mtval, TrapFrame* frame) {
-    (void)mepc;
-    (void)mtval;
+static void print_trap_values(const char* epc_name, uint32_t epc, const char* tval_name, uint32_t tval) {
+    Uart uart;
+    uart.print_str(" ");
+    uart.print_str(epc_name);
+    uart.print_str("=");
+    uart.print_hex(epc);
+    uart.print_str(" ");
+    uart.print_str(tval_name);
+    uart.print_str("=");
+    uart.print_hex(tval);
+    uart.print_str("\n");
+}
+
+void handle_interrupt(uint32_t code, uint32_t epc, uint32_t tval, TrapFrame* frame) {
+    (void)epc;
+    (void)tval;
     (void)frame;
     Uart uart;
 
     switch (static_cast<InterruptCode>(code)) {
+        case InterruptCode::SupervisorSoftwareInterrupt:
+            uart.print_str("[INTERRUPT] Supervisor Software Interrupt\n");
+            break;
+
         case InterruptCode::MachineSoftwareInterrupt:
             uart.print_str("[INTERRUPT] Machine Software Interrupt\n");
-            advance_mepc();
+            break;
+
+        case InterruptCode::SupervisorTimerInterrupt:
+            uart.print_str("[INTERRUPT] Supervisor Timer Interrupt\n");
             break;
 
         case InterruptCode::MachineTimerInterrupt:
             uart.print_str("[INTERRUPT] Machine Timer Interrupt\n");
-            advance_mepc();
+            break;
+
+        case InterruptCode::SupervisorExternalInterrupt:
+            uart.print_str("[INTERRUPT] Supervisor External Interrupt\n");
             break;
 
         case InterruptCode::MachineExternalInterrupt:
             uart.print_str("[INTERRUPT] Machine External Interrupt\n");
-            advance_mepc();
             break;
 
         default:
             uart.print_str("[INTERRUPT] code=");
             uart.print_hex(code);
-            uart.print_str(" mepc=");
-            uart.print_hex(mepc);
-            uart.print_str(" mtval=");
-            uart.print_hex(mtval);
-            uart.print_str("\n");
-            advance_mepc();
+            print_trap_values("epc", epc, "tval", tval);
             break;
     }
 }
 
-void handle_exception(uint32_t code, uint32_t mepc, uint32_t mtval, TrapFrame* frame) {
-    (void)mepc;
-    (void)mtval;
+void handle_exception(uint32_t code, uint32_t epc, uint32_t tval, TrapFrame* frame) {
+    (void)epc;
+    (void)tval;
     (void)frame;
     Uart uart;
 
     switch (static_cast<ExceptionCode>(code)) {
         case ExceptionCode::IllegalInstruction:
             uart.print_str("[EXCEPTION] Illegal Instruction\n");
-            advance_mepc();
+            advance_sepc();
             break;
 
         case ExceptionCode::Breakpoint:
             uart.print_str("[EXCEPTION] Breakpoint\n");
-            advance_mepc();
+            advance_sepc();
             break;
 
         case ExceptionCode::LoadAccessFault:
-        case ExceptionCode::StoreAMOAccessFault:
             uart.print_str("[EXCEPTION] Load Access Fault\n");
-            advance_mepc();
+            advance_sepc();
+            break;
+
+        case ExceptionCode::StoreAMOAccessFault:
+            uart.print_str("[EXCEPTION] Store/AMO Access Fault\n");
+            advance_sepc();
             break;
 
         case ExceptionCode::EnvironmentCallFromUMode:
         case ExceptionCode::EnvironmentCallFromSMode:
         case ExceptionCode::EnvironmentCallFromMMode:
             uart.print_str("[EXCEPTION] Environment Call\n");
-            advance_mepc();
+            advance_sepc();
             break;
 
         case ExceptionCode::InstructionPageFault:
         case ExceptionCode::LoadPageFault:
         case ExceptionCode::StoreAMOPageFault:
             uart.print_str("[EXCEPTION] Page Fault\n");
-            advance_mepc();
+            advance_sepc();
             break;
 
         default:
             uart.print_str("[EXCEPTION] code=");
             uart.print_hex(code);
-            uart.print_str(" mepc=");
-            uart.print_hex(mepc);
-            uart.print_str(" mtval=");
-            uart.print_hex(mtval);
-            uart.print_str("\n");
-            advance_mepc();
+            print_trap_values("epc", epc, "tval", tval);
+            advance_sepc();
             break;
     }
 }
 
-extern "C" void trap_handler(uint32_t mcause, uint32_t mepc, uint32_t mtval, TrapFrame* frame) {
-    bool is_interrupt = (mcause >> 31) & 1;
-    uint32_t code = mcause & 0x7FFFFFFF;
+extern "C" void supervisor_trap_handler(uint32_t scause, uint32_t sepc, uint32_t stval, TrapFrame* frame) {
+    bool is_interrupt = (scause >> 31) & 1;
+    uint32_t code = scause & 0x7FFFFFFF;
 
     if (is_interrupt) {
-        handle_interrupt(code, mepc, mtval, frame);
+        handle_interrupt(code, sepc, stval, frame);
     } else {
-        handle_exception(code, mepc, mtval, frame);
+        handle_exception(code, sepc, stval, frame);
     }
+}
 
-
+extern "C" void machine_trap_handler(uint32_t mcause, uint32_t mepc, uint32_t mtval, TrapFrame* frame) {
+    (void)frame;
+    Uart uart;
+    uart.print_str("[M-MODE TRAP] cause=");
+    uart.print_hex(mcause);
+    print_trap_values("mepc", mepc, "mtval", mtval);
+    while (1) {}
 }
